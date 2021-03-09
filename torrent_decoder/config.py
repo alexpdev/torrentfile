7373f43ba8d993b6305fe565fe587e81e23f01c2
alexspdev <alexprange@protonmail.com>
2021-03-08 21:27:28 -0800

    torrent_standard clean

--- torrent_decoder/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-import os
-torrent_folder = Path("A:\\torrents\\.torrents").resolve()
-v_folder = Path("V:\.torrents").resolve()
-search_folder = Path("A:\\torrents").resolve()
-data_folder = os.path.join(os.path.dirname(os.path.abspath(__file__)),"data")
