247de482d21d7d1aa59798bbdae9c328da940f2c
Alex <alexpdev@protonmail.com>
2021-08-03 11:36:06 -0700

    improved hashing process

--- torrentfile/metafile.py
+++ torrentfile/metafile.py
@@ -53,20 +53,136 @@ case).
 In the single file case, the name key is the name of a file, in the muliple
 file case, it's the name of a directory.
 """
+import os
+import sys
+import hashlib
+import datetime
+from pathlib import Path
+from torrentfile.bencode import Benencoder
+from torrentfile.piecelength import get_piece_length
+
+KIB = 2**10
+MIB = KIB**2
+GIB = KIB**3
+MIN_BLOCK = 2**14
+
+class InvalidDataType(Exception):
+    pass
+
+def sha1(data):
+    piece = hashlib.sha1()
+    piece.update(data)
+    return piece.digest()
+
+def sha256(data):
+    piece = hashlib.sha256()
+    piece.update(data)
+    return piece.digest()
+
+def md5(data):
+    piece = hashlib.md5()
+    piece.update(data)
+    return piece.digest()
+
+def get_pieces(paths,piece_size):
+    def hash_file(path, piece_length):
+        pieces = []
+        with open(path,"rb") as fd:
+            data = bytearray(piece_length)
+            while True:
+                length = fd.readinto(data)
+                if length == piece_length:
+                    pieces.append(sha1(data))
+                elif length > 0:
+                    pieces.append(sha1(data[:length]))
+                else:
+                    break
+        return bytes().join(pieces)
+    piece_layers = []
+    for path in paths:
+        piece = hash_file(path,piece_size)
+        piece_layers.append(piece)
+    return bytes().join(piece_layers)
+
+def walk_path(base,files,all_files):
+    for item in all_files:
+        if os.path.isfile(item):
+            desc = {'path': os.path.relpath(item, base).split(os.sep),
+                    'size' :os.path.getsize(item)}
+            files.append(desc)
+    return
+
+def folder_info(path,piece_size):
+    pass
 
 
-class TorrentMeta:
-    def __inti__(self,announce,info):
-        self.announce = announce
-        self.info = info
 
-class MetaInfo:
-    def __init__(self,name,piece_length,pieces,path,source=None,private=None,length=None,files=None):
-        self.name = name
-        self.piece_length = piece_length
-        self.pieces = pieces
-        self.length = length
-        self.files = files
-        self.path = path
-        self.private = private
-        self.source = source
+class TorrentFile:
+    def __init__(self,path=None,piece_length=None,creator=None,announce=None,private=None,source=None,length=None):
+        self._path = path
+        self.Path = Path(path)
+        self._is_file = self.Path._is_file()
+        self._name = self.Path.name
+        self._piece_length = piece_length
+        self._creator = creator
+        self._announce = announce
+        self._private = private
+        self._source = source
+        self._length = length
+        self._pieces = bytearray()
+        self._files = []
+        self.info = {}
+        self.meta = {}
+
+
+    def _get_filenames(self, directory, all_files):
+        names = os.listdir(directory)
+        names.sort(key=str.lower)
+        for name in names:
+            path = os.path.join(directory,name)
+            if os.path.isfile(path):
+                all_files.append(path)
+            elif os.path.isdir(path):
+                self._get_filenames(path, all_files)
+        return all_files
+
+    def assemble(self):
+        if not self._piece_length:
+            if not self._length:
+                self._length = self._path_size()
+            self._piece_length = get_piece_length(self._length)
+        directory = os.path.abspath(self._path)
+        all_files = self._get_filenames(directory, [])
+        self._info["piece length"] = self._piece_length
+        self._info["name"] = self._name.encode('uft-8')
+        if self._private:
+            self._info["private"] = self._private.encode("uft-8")
+        if self._creator:
+            self._info["created by"] = self._creator.encode("utf-8")
+        else:
+            self._info["created by"] = "pytorrentfile".encode("utf-8")
+        if self._source:
+            self._info["source"] = self._source.encode('utf-8')
+        self._info["creation date"] = datetime.datetime.isoformat(datetime.datetime.today()).encode('utf-8')
+        if self.is_file():
+            self._info['length'] = self._length
+            return self._info
+        walk_path(self.Path.name, self._files, all_files)
+        self._info["files"] = self._files
+        self._info['pieces'] = get_pieces(all_files,self._piece_size)
+        return self._info
+
+    def _folder_size(self,path):
+        size = 0
+        if path.is_file():
+            size += os.path.getsize(path)
+            return size
+        elif path.is_dir():
+            for item in path.iterdir():
+                size += self._folder_size(item)
+        return size
+
+    def _path_size(self):
+        if self.Path.is_file():
+            return os.path.getsize(self.Path)
+        return self._folder_size(self.Path)
