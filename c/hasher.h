2cc1f7545dd0ba0fc251720cf993d9cefdab59d9
Alex <alexpdev@protonmail.com>
2022-01-13 13:16:57 -0800

    updates to c modules

--- c/hasher.h
+++ c/hasher.h
@@ -1,15 +1,30 @@
 #ifndef HASHER_H
 #define HASHER_H
 
-#define BLOCK_SIZE 16384
+#include <stdint.h>
+
+typedef uint8_t uint8;
 
 typedef struct {
-    uint8_t *ptr;
+    uint8 *ptr;
     int size;
     int allocated;
     int logical;
 } HASH;
 
-HASH *HASHER(char **filelist, int piece_length);
+typedef struct { // Hash Result Object
+    uint8 *piece_layer;
+    uint8 *pieces_root;
+} HASHV2;
+
+typedef struct { // Hash Result Object
+    uint8 *piece_layerV3;
+    uint8 *pieces_root;
+    uint8 *hashv1;
+} HASHHYBRID;
+
+HASH *HASHER(char **filelist, unsigned piece_length);
+HASHV2 *HasherV2(char *path, unsigned piece_length);
+HASHHYBRID *HasherHybrid(char *path, unsigned piece_length);
 
 #endif
--- c/sha.h
+++ c/sha.h
@@ -1,5 +1,5 @@
-#ifndef SHA1_H
-#define SHA1_H
+#ifndef SHA_H
+#define SHA_H
 
 #include "stdint.h"
 
@@ -15,5 +15,19 @@ void SHA1Init(SHA1_CTX * context);
 void SHA1Update(SHA1_CTX * context, const uint8_t *data, uint32_t len);
 void SHA1Final(uint8_t digest[20], SHA1_CTX * context);
 void SHA1(uint8_t *hash_out, const uint8_t *str, int len);
+
+typedef struct {
+	uint8 *hash;
+	uint8 chunk[64];
+	uint8 *chunk_pos;
+	size_t space_left;
+	size_t total_len;
+	uint32_t h[8];
+} SHA256_CTX;
+
+void SHA256(uint8 *hash, const void *input, size_t len);
+void SHA256Init(SHA256_CTX *context, uint8 *hash);
+void SHA256Update(SHA256_CTX *context, const void *data, size_t len);
+void SHA256Final(SHA256_CTX *context);
 
 #endif
